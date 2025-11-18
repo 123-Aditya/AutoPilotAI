9edbb23ce0403fa6b60cf27a6258896e8b06194c
123-Aditya <mraditya345@gmail.com>
2025-11-18 20:31:58 +0530

    Adding Autopilot Agent project

---
+++ autopilot-agent/src/main/java/com/autopilot/autopilot_agent/AutopilotAgentApplication.java
@@ -0,0 +1,13 @@
+package com.autopilot.autopilot_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopilotAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopilotAgentApplication.class, args);
+	}
+
+}
---
+++ autopilot-agent/src/test/java/com/autopilot/autopilot_agent/AutopilotAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopilot.autopilot_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopilotAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
