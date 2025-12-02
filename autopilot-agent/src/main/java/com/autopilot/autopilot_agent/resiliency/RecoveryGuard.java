2c253830c85f04bef9165d9959e424fc6d442022
123-Aditya <mraditya345@gmail.com>
2025-12-03 00:49:09 +0530

    Adding circuit breaker RecoveryGuard class

---
+++ autopilot-agent/src/main/java/com/autopilot/autopilot_agent/resiliency/RecoveryGuard.java
@@ -0,0 +1,23 @@
+package com.autopilot.autopilot_agent.resiliency;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecoveryGuard {
+
+    private long lastRecoveryTime = 0;
+
+    @Value("${agent.recovery.cooldown.ms}")
+    private long cooldownMs;
+
+    public boolean canRecover() {
+        long now = System.currentTimeMillis();
+        return (now - lastRecoveryTime) > cooldownMs;
+    }
+
+    public void markRecoveryAttempted() {
+        lastRecoveryTime = System.currentTimeMillis();
+    }
+}
+
