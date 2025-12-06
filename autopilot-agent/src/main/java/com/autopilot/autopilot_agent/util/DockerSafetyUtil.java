57946aa27d1f8705297b6447fc3a950c225b1546
123-Aditya <mraditya345@gmail.com>
2025-12-06 20:20:16 +0530

    Adding utility DockerSafetyUtil

---
+++ autopilot-agent/src/main/java/com/autopilot/autopilot_agent/util/DockerSafetyUtil.java
@@ -0,0 +1,16 @@
+package com.autopilot.autopilot_agent.util;
+
+public class DockerSafetyUtil {
+
+    public static boolean containerExists(String name) {
+        try {
+            Process p = new ProcessBuilder(
+                    "docker", "inspect", name
+            ).start();
+            return p.waitFor() == 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+
