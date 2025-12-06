adf22084ab4950db441366ab2c1154770b79e966
123-Aditya <mraditya345@gmail.com>
2025-12-06 20:21:51 +0530

    Adding timeout utility ProcessUtil

---
+++ autopilot-agent/src/main/java/com/autopilot/autopilot_agent/util/ProcessUtil.java
@@ -0,0 +1,20 @@
+package com.autopilot.autopilot_agent.util;
+
+import java.util.concurrent.TimeUnit;
+
+public class ProcessUtil {
+
+    public static boolean run(ProcessBuilder builder, int timeoutSeconds)
+            throws Exception {
+
+        Process process = builder.start();
+        boolean finished = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
+
+        if (!finished) {
+            process.destroy();
+            return false;
+        }
+        return process.exitValue() == 0;
+    }
+}
+
